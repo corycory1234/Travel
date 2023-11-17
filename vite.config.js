0dc8d84c236055fb4881aac014b03bada6b72830
Kory CC <note10litenote10lite@gmail.com>
2023-11-17 12:55:40 +0800

    第2版 Vite.config.js, 修正base

--- vite.config.js
+++ vite.config.js
@@ -16,8 +16,5 @@ export default defineConfig({
   //     "@": fileURLToPath(new URL('./src', import.meta.url)),
   //   }
   // }
-  base: "./",
-  build: {
-    outDir: "docs"
-  },
+  base: "/Travel/",
 })
